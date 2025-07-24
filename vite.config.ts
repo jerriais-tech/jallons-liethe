d893f8da8ef15b14c3c5d8b8b5ed72a4b72c7832
Steve Le Roy Harris <steveleroyharris@gmail.com>
2025-07-24 14:14:17 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "J'allons liéthe",
+        short_name: "jallons-liethe",
+        description:
+          "Learn the vocabulary you need to understand Jèrriais literature",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
